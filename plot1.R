3ac5dac3caaaac91d4b79caffaa7adcd435797d0
chinrichsen <35776905+chinrichsen@users.noreply.github.com>
2018-08-24 07:36:14 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,18 @@
+#Get the file URL
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#Download the file zip file
 a <- download.file(fileUrl,destfile = "C:/Users/Carlos Hinrichsen/Data/w1c4.zip")
+#Unzip the file
 b <- unzip("C:/Users/Carlos Hinrichsen/Data/w1c4.zip")
+#Read the file
 y <- read.delim(b, sep=";", header = T)
+#Transform Date column into Date format
 y$Date <- strptime(y$Date, format= "%d/%m/%Y")
+#Subset the data considering only dates between 2007-02-01 and 2007-02-02
 w <- subset(y,y$Date$year==107 & y$Date$mon==1 & (y$Date$mday==1 |y$Date$mday==2))
+#Transform Global_active_power data into numeric
 w$Global_active_power <- as.numeric(levels(w$Global_active_power))[w$Global_active_power]
+#Save the local system (in case is not an english environment), for dates in english purposes
 old.system <- Sys.getlocale("LC_TIME")
 Sys.setlocale("LC_TIME", "English")
 png(file="plot1.png",width=480,height=480,units="px")
