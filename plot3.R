6b668ea2d74efb46482f2a3e32b36162c2b716db
chinrichsen <35776905+chinrichsen@users.noreply.github.com>
2018-08-24 07:36:22 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,22 @@
+#Get the file URL
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#Download the file zip file
 a <- download.file(fileUrl,destfile = "C:/Users/Carlos Hinrichsen/Data/w1c4.zip")
+#Unzip the file
 b <- unzip("C:/Users/Carlos Hinrichsen/Data/w1c4.zip")
+#Read the file
 y <- read.delim(b, sep=";", header = T)
+#Paste the columns Date and Time into the Date column, and then transform the column into Date format
 y$Date <- strptime(paste(y$Date,y$Time), format= "%d/%m/%Y %H:%M:%S")
+#Eliminate Time column
 y$Time <- NULL
+#Subset the data considering only dates between 2007-02-01 and 2007-02-02
 w <- subset(y,y$Date$year==107 & y$Date$mon==1 & (y$Date$mday==1 |y$Date$mday==2))
+#Transform Sub_metering_1, Sub_metering_2 and Sub_metering_3 data into numeric
 w$Sub_metering_1 <- as.numeric(levels(w$Sub_metering_1))[w$Sub_metering_1]
 w$Sub_metering_2 <- as.numeric(levels(w$Sub_metering_2))[w$Sub_metering_2]
 w$Sub_metering_3 <- as.numeric(levels(w$Sub_metering_3))[w$Sub_metering_3]
+#Save the local system (in case is not an english environment), for dates in english purposes
 old.system <- Sys.getlocale("LC_TIME")
 Sys.setlocale("LC_TIME", "English")
 png(file="plot3.png",width=480,height=480,units="px")
